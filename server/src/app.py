28cd8bce6bad9eac73262c093a4ba4aafe1f57f8
twoodrow <twoody2007@gmail.com>
2019-05-25 14:29:50 -0700

    formatting

--- server/src/app.py
+++ server/src/app.py
@@ -3,6 +3,8 @@ from flask import Flask
 from .config import app_config
 from .models import db, bcrypt
 from .views.UserView import user_api as user_blueprint
+
+
 def create_app(env_name):
     app = Flask(__name__)
     app.config.from_object(app_config[env_name])
