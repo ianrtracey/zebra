c6a3662bb85bc9c8bee92a9a396dff7d440135b7
twoodrow <twoody2007@gmail.com>
2019-05-25 17:01:06 -0700

    adds CORS to app

--- server/src/app.py
+++ server/src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from .config import app_config
 from .models import db, bcrypt
@@ -7,6 +8,7 @@ from .views.UserView import user_api as user_blueprint
 
 def create_app(env_name):
     app = Flask(__name__)
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
     app.config.from_object(app_config[env_name])
 
     bcrypt.init_app(app)
